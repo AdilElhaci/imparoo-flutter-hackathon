8d5f8da6cde7b6068decb78d09872185ba36e81d
AdilElhaci <adelalhajji97@gmail.com>
2021-02-07 03:25:40 +0300

    button images names changed

---
+++ lib/core/constants/button_images.dart
@@ -0,0 +1,19 @@
+class ButtonImages {
+  static const DERSLERBUTTON = 'assets/images/sayfalar-buton-dersler 1.png';
+  static const DERSLERBUTTONCLICKED =
+      'assets/images/sayfalar-buton-dersler-2.png';
+
+  static const SORULARBUTTON = 'assets/images/sayfalar-buton-sorular-1.png';
+
+  static const SORULARBUTTONCLICKED =
+      'assets/images/sayfalar-buton-sorular-2 1.png';
+
+  static const KAYITBUTTON = 'assets/images/sayfalar-buton-kayit 1.png';
+
+  static const KAYITBUTTONCLICKED =
+      'assets/images/sayfalar-buton-kayit-2 1.png';
+
+  static const ODEVLERBUTTON = 'assets/images/sayfalar-buton-odevler 1.png';
+
+  static const BASARIMBUTTON = 'assets/images/sayfalar-buton-basarim 1.png';
+}
